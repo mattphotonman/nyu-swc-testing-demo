19d307aeb9cbc6bf73a6df71b57c278922e202de
Matthew Lightman <matt@photon.ipv6berry.com>
2014-03-18 14:35:43 -0400

    Oops, forgot to add the actual files.

---
+++ coordinates_to_region.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""
+This module was created to demonstrate testing at the
+03/2014 NYU Software Carpentry Bootcamp. The functions
+below contain documentation on their use.
+"""
+
+def coordinates_to_region():
+    """
+    Takes a set of coordinates and returns the
+    regions in which it lies.
+    """
+    pass
+
+def main():
+    """This is the main controller for my module."""
+    pass
+
+if __name__=='__main__':
+    main()
---
+++ test_coordinates_to_region.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+"""This is the nosetest module for the coordinates_to_region.py"""
+
+def test_foo():
+    """Bar"""
+    pass
